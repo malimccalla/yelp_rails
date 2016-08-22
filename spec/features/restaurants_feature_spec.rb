39d2ad515247fb8411befef6808a3c8ca01254a9
Jack Oddy <jackoddy91@gmail.com>
2016-08-22 17:30:55 +0100

    adds the D from CRUD into the mix

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -63,4 +63,16 @@ feature 'restaurants' do
 
   end
 
+  context 'deleting restaurants' do
+
+    before { Restaurant.create name: 'Nando\'s', description: 'Super Yumma' }
+
+    scenario 'remotes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete Nando\'s'
+      expect(page).not_to have_content 'Nando\'s'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
+
 end

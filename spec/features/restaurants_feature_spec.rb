3bcd106ceb6e325db72df7a2dce69dbf84e1dc74
Jack Oddy <jackoddy91@gmail.com>
2016-08-22 15:50:50 +0100

    adds the C in CRUD to the mix

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -12,13 +12,23 @@ feature 'restaurants' do
 
   context 'restaurants have been added' do
     before do
-      Restaurant.create(name: 'Jack & Mali\'s Meat Hook')
+      Restaurant.create(name: 'nandos')
     end
-
     scenario 'display restaurants' do
       visit '/restaurants'
-      expect(page).to have_content('Jack & Mali\'s Meat Hook')
+      expect(page).to have_content('nandos')
       expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context ' creating restaurants' do
+    scenario ' prompt user to fill out a form and then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'nandos'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'nandos'
+      expect(current_path).to eq '/restaurants'
     end
   end
 

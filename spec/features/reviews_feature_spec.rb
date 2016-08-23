0fdfa778619913e697bc8d2e94e171f07a19fb58
Mali Michael <malimccalla@gmail.com>
2016-08-23 20:21:51 +0100

    Adds review form

---
+++ spec/features/reviews_feature_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'reviewing' do
+
+  before(:each) do
+    Restaurant.create(name: 'KFC')
+  end
+
+  scenario 'allows users to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content 'so so'
+  end
+
+end
